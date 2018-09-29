c5a4a4c0520f39717f65ccbd7dc13e66e0d5db87
jeremiahvpratt <pratt@cooper.edu>
2018-09-29 05:51:15 -0400

    forgot abt the wormhole check

--- src/checkGotWormholed.py
+++ src/checkGotWormholed.py
@@ -1,7 +1,8 @@
 import numpy as np
 
 def checkGotWormholed(xOld,yOld,xNew,yNew):
-    if np.sqrt((xNew-xOld)**2 + (yNew-yOld)**2) > 100:
+    # if np.sqrt((xNew-xOld)**2 + (yNew-yOld)**2) > 100:
+    if (np.abs(xNew-xOld) > 100) and (np.abs(yNew-yOld) > 100):
         return 1
     else:
         return 0
