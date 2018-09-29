269ff0358f80aa49671649d86792bcd87f5e128e
[Joseph Bentivegna] <[jbentivegna15@gmail.com]>
2018-09-29 05:36:14 -0400

    fixed decision algo

--- src/decideDirection.py
+++ src/decideDirection.py
@@ -6,7 +6,26 @@ def decideDirection(curInfo, newx, newy):
     x = float(curInfo['x'])
     y = float(curInfo['y'])
 
-    if (math.hypot(x-newx, y-newy)) > (math.hypot(x+(float(globals.MAPWIDTH)-newx), y+(float(globals.MAPHEIGHT)-newy))):
-        return -(float(globals.MAPWIDTH)-newx), -(float(globals.MAPWIDTH)-newy)
-    else:
-        return newx, newy
+    if x >= 5000 and y >= 5000:
+        if (math.hypot(x-newx, y-newy)) > (math.hypot(x-(newx+float(globals.MAPWIDTH)), y-(newy+float(globals.MAPHEIGHT)))):
+            return (newx+float(globals.MAPWIDTH)), (newy+float(globals.MAPHEIGHT))
+        else:
+            return newx, newy
+
+    if x >= 5000 and y <= 5000:
+        if (math.hypot(x-newx, y-newy)) > (math.hypot(x-(newx+float(globals.MAPWIDTH)), y-(newy-float(globals.MAPHEIGHT)))):
+            return (newx+float(globals.MAPWIDTH)), (newy-float(globals.MAPHEIGHT))
+        else:
+            return newx, newy
+
+    if x <= 5000 and y >= 5000:
+        if (math.hypot(x-newx, y-newy)) > (math.hypot(x-(newx-float(globals.MAPWIDTH)), y-(newy+float(globals.MAPHEIGHT)))):
+            return (newx+float(globals.MAPWIDTH)), (newy+float(globals.MAPHEIGHT))
+        else:
+            return newx, newy
+
+    if x <= 5000 and y<= 5000:
+        if (math.hypot(x-newx, y-newy)) > (math.hypot(x-(newx-float(globals.MAPWIDTH)), y-(newy-float(globals.MAPHEIGHT)))):
+            return (newx-float(globals.MAPWIDTH)), (newy-float(globals.MAPHEIGHT))
+        else:
+            return newx, newy
